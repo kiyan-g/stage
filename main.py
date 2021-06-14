a49bc39e056aae69abfee9840dadbd880e93f2ac
Kiyan Ghoumari <kiyan.ghoumari@eleve.ensai.fr>
2021-06-14 18:46:14 +0200

    étude bitcoin

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from ta import add_all_ta_features
+import pandas_datareader.data as web
+import talib
+from talib import RSI, BBANDS
 from sklearn.linear_model import LinearRegression
 
-# Essai basic
-print('Hello world')
 
 # Figure bitcoin
 bitcoin = pd.read_csv('BTC-EUR.csv', index_col='Date', parse_dates= True)
+bitcoin = bitcoin.dropna()
 bitcoin.head()
 bitcoin['Close'].plot()
-plt.show()
+#plt.show()
 
-# Essai rég linéaire
-np.random.seed(0)
-m = 100  #100 samples
-x = np.linspace(0, 10, m).reshape(m, 1)
-y = x + np.random.randn(m, 1)
 
-plt.scatter(x, y)
+# Essai ta-lib avec moyenne glissante
+Bc = talib.SMA(bitcoin['Close'].values, timeperiod=30)
+plt.plot(Bc, label='Bitcoin')
+plt.plot(bitcoin['Close'].values,label='Price')
+plt.legend(loc='best')
+#plt.show()
 
-model = LinearRegression()
-model.fit(x, y)
-model.score(x, y)
-plt.show()
+
+# Essai ta-lib 2 avec RSI et BBands
+up, mid, low = BBANDS(bitcoin['Close'].values, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+rsi = RSI(bitcoin['Close'].values, timeperiod=14)
+print("RSI (first 10 elements)\n", rsi[14:24])
+
+
+def bbp_fonc(donnees):
+    bbp=[]
+    for i in range (len(donnees['Close'])):
+        bbp += [(donnees['Close'][i] - low[i]) / (up[i] - low[i])]
+    return bbp
+
+#print(bbp_fonc(bitcoin)[20:40])
+
+# Création d'un dataframe du RSI pour diff valeurs de 'timeperiod'
+def rsi_frame(donnees, n):
+    Rsi_f = []
+    c = range(2, n)
+    for i in range(2,n):
+        Rsi_f += RSI(donnees['Close'].values, timeperiod=i)
+    Rsi_ft = np.transpose(Rsi_f)
+    Rsi_frame = pd.DataFrame(Rsi_ft, columns=c ) 
+    return Rsi_frame
+
+Rindice = rsi_frame(bitcoin, 20)
+Rindice.head()
+
+#index=donnees['Date']
+
+
+##mettre bitcoin et moyenne glissante sur le mm graphique ac la date en abs
+##resoudre le pb du Rsi_frame
+##faire les frames des autres indicateurs du mail
